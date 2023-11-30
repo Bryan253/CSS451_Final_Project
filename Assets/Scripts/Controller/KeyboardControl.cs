4f7be62bcd06bcdb19775dceacb8ad67751414ed
Bryan253 <bryan530@uw.edu>
2023-11-30 15:27:38 -0800

    Added some basic blueprint for the player

---
+++ Assets/Scripts/Controller/KeyboardControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControl : MonoBehaviour
+{
+    // Insert joints and base for heirchy to move or rotate
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+        HeadMovement();
+    }
+
+    void PlayerMovement()
+    {
+        // Do something based on key pressed
+    }
+
+    void HeadMovement()
+    {
+        // Key press to move head
+    }
+}
